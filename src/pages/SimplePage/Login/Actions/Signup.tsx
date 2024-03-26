c896b0ff96e83e1579d722e842cf09451f2b7e81
brugondim <90776432+brugondim@users.noreply.github.com>
2024-03-26 12:20:37 -0300

    Update Signup.tsx

--- src/pages/SimplePage/Login/Actions/Signup.tsx
+++ src/pages/SimplePage/Login/Actions/Signup.tsx
@@ -82,117 +82,120 @@ export function Signup() {
   }
 
   return (
-    <div className="p-6 bg-white shadow-md rounded-lg mb-3">
-      <h2 className="text-2xl font-semibold mb-4 text-center">Cadastre-se</h2>
-      <form onSubmit={handleLogin} className="flex flex-wrap">
-      <div className="w-full sm:w-1/2 px-2">
-          <TextInput
-            label="Name"
-            type="text"
-            id="name"
-            value={Name}
-            onChange={(e) => setName(e.target.value)}
-            placeholder={language.nameButtonPlaceholder}
-          />
-        </div>
-        <div className="w-full sm:w-1/5 px-2">
-          <TextInput
-            label="CPF/CNPJ"
-            type="text"
-            id="cpfCnpj"
-            value={cpfCnpj}
-            onChange={(e) => setCpfCnpj(e.target.value)}
-            placeholder="Informe o CPF ou CNPJ"
-          />
-        </div>
-        <div className="w-full sm:w-1/4 px-2">
-          <TextInput
-            label="Telefone"
-            type="text"
-            id="phone"
-            value={phone}
-            onChange={(e) => setPhone(e.target.value)}
-            placeholder="Informe o telefone"
-          />
-        </div>
-        <div className="w-full sm:w-1/2 px-2">
-          <TextInput
-            label="Email"
-            type="email"
-            id="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            placeholder={language.emailButtonPlaceholder}
-          />
-        </div>
-        <div className="w-full sm:w-1/5 px-2">
-          <TextInput
-            label={language.passwordLabel}
-            type="password"
-            id="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            placeholder={language.passwordButtonPlaceholder}
-          />
-           </div>
-
-        <div className="w-full sm:w-1/4 px-2">
-          <TextInput
-            label="CEP"
-            type="text"
-            id="cep"
-            value={cep}
-            onChange={handleCEPChange}
-            placeholder="Informe o CEP"
-          />
-        </div>
-        <div className="w-full sm:w-1/3 px-2">
-          <TextInput
-            label="Endereço"
-            type="text"
-            id="Adress"
-            value={address}
-            onChange={(e) => setAddress(e.target.value)}
-            placeholder="Informe o nome da Rua"
-          />
-        </div>
-        <div className="w-full sm:w-1/6 px-2">
-          <TextInput
-            label="Estado"
-            type="text"
-            id="state"
-            value={state}
-            onChange={(e) => setState(e.target.value)}
-            placeholder="Informe o estado"
-          />
-        </div>
-        <div className="w-full sm:w-1/3 px-2">
-          <TextInput
-            label="Cidade"
-            type="text"
-            id="city"
-            value={city}
-            onChange={(e) => setCity(e.target.value)}
-            placeholder="Informe a cidade"
-          />
-        </div>
-        
-        <div className="flex w-full justify-between px-2 mt-3">
-          <button
-            type="button"
-            className="bg-green-500 text-white p-2 rounded-md hover:bg-blue-600"
-            onClick={() => navigate('/')}
-          >
-            Voltar
-          </button>
-          <button
-            type="submit"
-            className="bg-green-500 text-white p-2 rounded-md hover:bg-blue-600"
-          >
-            {language.LoginPageButton}
-          </button>
-        </div>
-      </form>
+    <div className="flex justify-center items-center h-screen">
+      <div className="p-6 bg-white shadow-md rounded-lg mb-3 w-full sm:w-3/4 md:w-1/2 lg:w-1/3 xl:w-1/4">
+        <h2 className="text-2xl font-semibold mb-4 text-center">Cadastre-se</h2>
+        <form onSubmit={handleLogin} className="flex flex-wrap">
+          <div className="w-full px-2 sm:w-1/1">
+            <TextInput
+              label="Name"
+              type="text"
+              id="name"
+              value={Name}
+              onChange={(e) => setName(e.target.value)}
+              placeholder={language.nameButtonPlaceholder}
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="CPF/CNPJ"
+              type="text"
+              id="cpfCnpj"
+              value={cpfCnpj}
+              onChange={(e) => setCpfCnpj(e.target.value)}
+              placeholder="Informe o CPF ou CNPJ"
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="Telefone"
+              type="text"
+              id="phone"
+              value={phone}
+              onChange={(e) => setPhone(e.target.value)}
+              placeholder=" Telefone"
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="Email"
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              placeholder={language.emailButtonPlaceholder}
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label={language.passwordLabel}
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              placeholder={language.passwordButtonPlaceholder}
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="CEP"
+              type="text"
+              id="cep"
+              value={cep}
+              onChange={handleCEPChange}
+              placeholder="Informe o CEP"
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="Endereço"
+              type="text"
+              id="Adress"
+              value={address}
+              onChange={(e) => setAddress(e.target.value)}
+              placeholder="Informe o nome da Rua"
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="Estado"
+              type="text"
+              id="state"
+              value={state}
+              onChange={(e) => setState(e.target.value)}
+              placeholder="Informe o estado"
+            />
+          </div>
+          <div className="w-full px-2 sm:w-1/2">
+            <TextInput
+              label="Cidade"
+              type="text"
+              id="city"
+              value={city}
+              onChange={(e) => setCity(e.target.value)}
+              placeholder="Informe a cidade"
+            />
+          </div>
+          
+          <div className="w-full px-2 mt-3">
+            <div className="flex justify-between">
+              <button
+                type="button"
+                className="bg-green-500 text-white p-2 rounded-md hover:bg-blue-600"
+                onClick={() => navigate('/')}
+              >
+                Voltar
+              </button>
+              <button
+                type="submit"
+                className="bg-green-500 text-white p-2 rounded-md hover:bg-blue-600"
+              >
+                {language.LoginPageButton}
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
     </div>
   );
 }
